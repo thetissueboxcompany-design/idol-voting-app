05e297c43076d40dbd94a50f89c73eaf324ea99d
idol-voting-app <thetissueboxcompany@gmail.com>
2025-08-07 22:31:44 +0000

    Update next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // HIGHLIGHT: Add this line to enable static site generation
+  output: 'export',
+};
+
+module.exports = nextConfig;
